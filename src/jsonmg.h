072d7d3de5810f6381b5624f51c33c635f7454d0
TheRootED24 <TheRootEd24@gmail.com>
2024-07-31 13:58:12 -0400

    Update jsonmg.h
    edits for new lua parser logic

--- src/jsonmg.h
+++ src/jsonmg.h
@@ -40,9 +40,13 @@ enum
 
 typedef struct
 {
+        int depth;
         int ttype;
+        int rtype;
         int ptype;
+        int pindex;
         int ctype;
+        int cindex;
 
         int ktype;
         int vtype;
@@ -53,27 +57,31 @@ typedef struct
         bool isChild;
         bool isNested;
         bool unSuppoted;
+        bool parentMode;
+        bool childMode;
+        bool nestMode;
         char *json;
         const char *key;
 } parseLua;
 
+static char buf[MAX_JSON_LEN];
+static int depth = 0;
 
-
+parseLua pLr = {.ttype = -1, .ptype = -1, .ctype = -1, .ktype = -1, .vtype = -1, .nkey = 0, .isRoot = true, 
+                                                .parentMode = true, .childMode = false, .isParent = false, .isChild = false, .isNested = false, .json = buf, .key = NULL};
 static double num = 0;
 static bool b;
 static long l;
 static char *sstr;
 
-
 int nextClose[MAX_DEPTH];
-
 bool isArrStr = false, isArrNum = false, isArrObj = false;
 
-static char buf[MAX_JSON_LEN];
 static const char *nextJson = ",";
 static const char *kvString = "%s:\"%s\"";
+static const char *nestedkvString = "{%s:\"%s\"}";
 static const char *kvNumber = "%s:%f";
-static const char *idxNumber = "%f:%f";
+static const char *nestedkvNumber = "{%s:%f}";
 static const char *newKeyArray = "%s:[";
 static const char *newArray = "[";
 static const char *arrayString = "\"%s\"";
@@ -81,7 +89,7 @@ static const char *arrayNumber = "%f";
 static const char *endArray = "]";
 static const char *newObject = "{";
 static const char *newKeyObject = "%s:{";
-static const char *nestedKeyObject = "{%s:";
+static const char *nestedKeyObject = "{%s:{";
 static const char *endObject = "}";
 
 #endif /* JSONMG_H */

5101597f140569dbc9cc72a73a05439a27068ffa
TheRootED24 <TheRootEd24@gmail.com>
2024-08-11 14:10:32 -0400

    Update jsonmg.c

--- src/jsonmg.c
+++ src/jsonmg.c
@@ -1,7 +1,7 @@
 /*
  * jsonmg - JSON parsing library for lua
  *
- *   Copyright (C) 2024 TheRootED24
+ *   Copyright (C) 2024 TheRootED24 <TheRootED24@gmail.com>
  *
  *   This program is free software: you can redistribute it and/or modify it
  *   under the terms of the GNU General Public License as published by the Free Software Foundation, 
